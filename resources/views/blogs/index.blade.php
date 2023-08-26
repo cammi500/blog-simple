1d87a973715ad753619c6b7a678502376454af82
THIRI AUNG <ss.sonali2003@gmail.com>
2023-08-26 23:05:28 +0630

    try

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,23 +1,6 @@
-
 <x-layout>
-    <div class=" container">
-        <form action="/" method="GET">
-            <label for="name">Search :</label>
-            <input  value="{{request('query')}}" type="text" name="query" >
-            <button type="submit">Search</button>
-        </form>
-
-        {{-- @if ($blogs->count()) --}}
-        @forelse($blogs as $blog ) 
-       <x-blog-card
-        :loop='$loop'
-         :blog='$blog'/>
-         @empty
-         <p>No blog found</p>
-        @endforelse
-
-        {{-- @else
-        <p>No blog found</p>
-        @endif --}}
-    </div>
-</x-layout>
+    <x-navbar/>
+     <x-hero/>
+      <x-blog-section/>
+      <x-subscribe/>
+</x-layout>   
